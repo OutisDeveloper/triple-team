36659f504e07d090cb2f14f72a692289c8e86cb6
OutisDeveloper <behruz234o@gmail.com>
2023-04-04 00:04:10 +0500

    readme-fayl-qoshldi

---
+++ README.txt
@@ -0,0 +1,6 @@
+
+  Navbar va Home qismini-----------Behruz Jurayev bajardi
+
+  About qismini--------------------Abdullayev Mohir bajardi
+
+  Contact Us va Footer qismini-----Abduvorisbek Shobilolov
--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@
 window.addEventListener("scroll", function () {
   shrink();
 });
-let navbar = document.getElementById("navig");
+
+let navbar = document.getElementById("nav-bar");
+
 function shrink() {
   if (scrollY > 100) {
     navbar.classList.add("navbar-shrink");
